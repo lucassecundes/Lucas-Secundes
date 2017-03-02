9f02aeb51226a33b3ffb1acf24aeeb178d9d65a9
lucassecundes <lssecundes@gmail.com>
2017-03-02 19:45:27 -0400

    Update VariavelTipo.java

--- programacaoI-2017-1-master/src/main/java/br/estacio/terenciani/programacao/VariavelTipo.java
+++ programacaoI-2017-1-master/src/main/java/br/estacio/terenciani/programacao/VariavelTipo.java
@@ -1,21 +1,36 @@
-
 package br.estacio.terenciani.programacao;
 
 
 public class VariavelTipo {
 
+   
+
+        
+       public static void main(String[] args) {
+        
+        //imprime();
+        //variavel inteira chamada "x" recebe o valor 10
+        int x = 10;
+        //variavel inteira chamada "y" recebe o valor 4
+        int y = 4;
+        
+        int resultado;
+        
+        resultado = multiplica(x,y);
+        
+        System.out.println("O resultado eh: " + resultado);
+        
+    }
     
-    public static void main(String[] args) {
-        System.out.print("-----!");
-        System.out.print("Teste");
-        System.out.print("-----!");
-        
-        System.out.print("Estou Brincando de funções!");
-        
+    public static int multiplica (int x, int y){
+        return x * y;
     }
-    public void imprime () {
-        System.out.print("-----!");
-        System.out.print("Teste");
-        System.out.print("-----!");
+    
+     public static void imprime(){
+        System.out.println("-----");
+        System.out.println("Teste");
+        System.out.println("-----");
     }
+   
+    
 }
